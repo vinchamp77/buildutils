acfdc5764b86e213d5ecfcd823c955c71a872c66
vinchamp77 <vinchamp77@gmail.com>
2022-11-11 09:23:31 +1100

    add singleVariant publishing option

--- buildutils/build.gradle.kts
+++ buildutils/build.gradle.kts
@@ -32,6 +32,12 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+            withJavadocJar()
+        }
+    }
 }
 
 dependencies {
